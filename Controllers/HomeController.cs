59899f664765456cd56ed48940f5cd81051130df
Michael Redifer <mikeredifer@yahoo.com>
2022-03-07 14:12:59 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonalSiteMVC.Models;
+using System.Net.Mail;
+using System.Configuration; 
+using System.Net;
+
+namespace PersonalSiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult ContactAjax(ContactViewModel cvm)
+        {
+
+                string body = $"{cvm.Name} has sent you the following message:<br/>" +
+                    $"{cvm.Message} from: {cvm.Email}.";
+                //Message Object
+                MailMessage mm = new MailMessage(
+                //FROM address - email must be on host - creds stored in Web.config
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                //TO - email doesn't have to be on host - creds stored in Web.config
+                ConfigurationManager.AppSettings["EmailTo"].ToString(),
+                //email subject
+                cvm.Subject,
+                body);
+
+                mm.IsBodyHtml = true;
+                mm.Priority = MailPriority.High;
+                mm.ReplyToList.Add(cvm.Email);
+
+                SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+                client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+                try
+                {
+                    //send email
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = $"We're sorry your request could not be completed at this time" +
+                        $"Please try again later.\nError Message: {ex.StackTrace}";
+                    
+                }
+                    return Json(cvm);
+            
+        }
+
+
+        
+    }
+}
---
+++ Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteMVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="*Your name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="*Your email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="*A Subject is required")]
+        public string Subject { get; set; }
+        [UIHint("MultilineText")]
+        [Required(ErrorMessage = "*A message is required.")]
+        public string Message { get; set; }
+
+        
+    }
+}
